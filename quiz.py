a36a87fd80c95860d51b41116b4d675fd222699b
yuya-18999 <ew.btazbx@gmail.com>
2021-11-25 22:20:35 +0900

    init commit

---
+++ quiz.py
@@ -0,0 +1,41 @@
+print('ようこそ　ゲームクイズへ！！')
+
+playing = input('遊びますか？ y or n')
+
+if playing.lower() == 'y':
+    print('スタート!!')
+else:
+    print('バイバイ')
+    quit()
+
+score = 0
+
+answer = input('FPSはなんの略称？')
+if answer.lower() == 'first-person shooting' or answer == 'ファーストパーソンシューティング':
+    print('正解')
+    score += 1
+else:
+    print('不正解')
+
+answer = input('TPSはなんの略称？')
+if answer.lower() == 'third-person shooting' or answer == 'サードパーソンシューティング':
+    print('正解')
+    score += 1
+else:
+    print('不正解')
+
+answer = input('RPGはなんの略称？')
+if answer.lower() == 'role-playing game' or answer == 'ロールプレイングゲーム':
+    print('正解')
+    score += 1
+else:
+    print('不正解')
+
+answer = input('RTSはなんの略称？')
+if answer.lower() == 'real-time strategy' or answer == 'リアルタイムストラテジー':
+    print('正解')
+    score += 1
+else:
+    print('不正解')
+
+print('あなたの成績' + str(score), '正解率' + str((score / 4) * 100) + '%')
